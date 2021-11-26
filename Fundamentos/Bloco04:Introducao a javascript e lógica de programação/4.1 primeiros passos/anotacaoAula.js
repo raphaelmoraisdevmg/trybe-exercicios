76bd15f8886dc0367caac36c63f0f2a1a4ea3662
Raphael Almeida de Morais <raphaelmoraisdev@gmail.com>
2021-11-26 16:04:42 -0300

    Anotacoes e exercicios finalizados

--- Fundamentos/Bloco04:Introducao a javascript e lógica de programação/4.1 primeiros passos/anotacaoAula.js
+++ Fundamentos/Bloco04:Introducao a javascript e lógica de programação/4.1 primeiros passos/anotacaoAula.js
@@ -7,3 +7,35 @@ pi=3.15;
 console.log(age);
 /*conecito de cammel case arpesentado e a variáveis são case sensitive */
 console.log(fullName);
+
+//tipos primitivos e tipagem dinamica
+let movie= "Avengers"; // string
+let score= 10.89 //number literal
+let isValid=true; //boolean
+let name; // undefined
+let color=null; //null
+//trypeOf() descobre qual o tipo da variavel passada por parametro
+//aritimética
+let salary= 3500;
+/* console.log(salary+salary);
+console.log(salary-salary);
+console.log(salary*salary);
+console.log(salary/salary);
+console.log(3**3); */
+salary++
+console.log(salary);
+salary--
+console.log(salary);
+
+//IF/ELSE
+let trybe=16.42;
+if(trybe >= 14 && trybe < 14.00){
+    console.log("Esquenta");
+}else if (trybe >= 16.30 && trybe < 17.50){
+    console.log("Aula ao vivo");
+}else if(trybe >=19.40 && trybe < 20){
+    console.log("Fechamento");
+}
+else{
+    console.log("Fora dos momentos síncronos");
+}
---
+++ Fundamentos/Bloco04:Introducao a javascript e lógica de programação/4.1 primeiros passos/tipos.js
@@ -0,0 +1,7 @@
+let base= 5;
+let heigth= 8;
+let area= 0;
+area=base*heigth;
+let permeter=heigth*2;
+console.log(area);
+console.log(permeter)
