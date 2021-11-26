7e4073826056852acaa60276edd100b5e52053bb
Raphael Almeida de Morais <raphaelmoraisdev@gmail.com>
2021-11-26 13:03:11 -0300

    criando a pasta bloco4 e 4.1 junto com os primeiros exercicios do bloco

---
+++ Fundamentos/Bloco04:Introducao a javascript e lógica de programação/4.1 primeiros passos/Variaveis.js
@@ -0,0 +1,12 @@
+let myName="Raphael Almeida de Morais";
+const birthCity="Uberaba-MG"
+let birthYear=1993;
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+birthYear=2030;
+console.log(birthYear);
+/*birthCity=Curitiba-pr;
+console.log(birthCity); 
+o código digitado nesse comentário está errado, quando executado retornara erro de constante pois uma constante só pode ter um valor atribuído
+*/ 
---
+++ Fundamentos/Bloco04:Introducao a javascript e lógica de programação/4.1 primeiros passos/anotacaoAula.js
@@ -0,0 +1,9 @@
+let age=20;
+let fullName="Hulk";
+/* uma const não pode ter seu valor alterado
+const pi=3.14;
+pi=3.15;
+*/
+console.log(age);
+/*conecito de cammel case arpesentado e a variáveis são case sensitive */
+console.log(fullName);
