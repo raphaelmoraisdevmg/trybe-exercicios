bc2a16a01d20619b14f69f01cf86367cb105f4f9
Raphael Almeida de Morais <raphaelmoraisdev@gmail.com>
2021-12-20 20:39:18 -0300

    exercicio 5.2 finalizado

--- Fundamentos/Bloco05:DOM,Eventos e Web Storage/5.2 trabalhando com elementos/exercicio/script.js
+++ Fundamentos/Bloco05:DOM,Eventos e Web Storage/5.2 trabalhando com elementos/exercicio/script.js
@@ -1,6 +1,8 @@
 //1 Adicione a tag h1 com o texto Exercício 5.2 - JavaScript DOM como filho da tag body;
 let titulo=document.createElement('h1');
 titulo.innerText="Exercício 5.2 - JavaScript DOM";
+//1 Adicione a classe title na tag h1 criada;  PARTE 2  
+titulo.className="title";
 const corpo=document.body;
 corpo.appendChild(titulo);
 //2 Adicione a tag main com a classe main-content como filho da tag body ;
@@ -42,19 +44,33 @@ let valores=[
     'Dez'
 ]
 let listaValores=document.createElement('ul');
+listaValores.className="lista"
 let valoresitem='';
 for (let index = 0; index<valores.length;index +=1 ){
     valoresitem=document.createElement('li');
+    valoresitem.className="item";
     valoresitem.innerHTML=valores[index];
     listaValores.appendChild(valoresitem);
 }
 conteudoADireita.appendChild(listaValores);
 //9 Adicione 3 tags h3 , todas sendo filhas do main criado no passo 2.
-palavras=['DOM','JAVA SCRIPT','CSS'];
+let palavras=['DOM','JAVA SCRIPT','CSS'];
 
 
 for(let index=0;index<palavras.length; index+=1){
     let subTitulos=document.createElement('h3');
     subTitulos.innerHTML=palavras[index];
+    //2 Adicione a classe description nas 3 tags h3 criadas; PARTE 2
+    subTitulos.className="description"
     principal.appendChild(subTitulos)
 }
+//parte 2
+//3 Remova a section criado no passo 5 (aquele que possui a classe left-content ). Utilize a função .removeChild() ;
+seccaoUm.removeChild(conteudoAEsquerda)
+//4 Centralize a section criado no passo 6 (aquele que possui a classe right-content ). Dica: para centralizar, basta configurar o margin-right: auto da section ;
+conteudoADireita.style.marginRight="auto";
+//5 Troque a cor de fundo do elemento pai da section criada no passo 3 (aquela que possui a classe center-content ) para a cor verde;
+seccaoUm.style.backgroundColor="green";
+//6 Remova os dois últimos elementos ( nove e dez ) da lista criada no passo 8.
+listaValores.lastChild.remove();// remove o ultimo filho nesse momento o 10
+listaValores.lastChild.remove();// remove o ultimo filho nesse momento o 9
