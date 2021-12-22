6d55abf70ef04f4bb6144145c37278a968a319e0
Raphael Almeida de Morais <raphaelmoraisdev@gmail.com>
2021-12-21 21:43:33 -0300

    mais exercicios resolvidos no 5.3

--- Fundamentos/Bloco05:DOM,Eventos e Web Storage/5.3 JavaScript- Eventos/exercicio/script.js
+++ Fundamentos/Bloco05:DOM,Eventos e Web Storage/5.3 JavaScript- Eventos/exercicio/script.js
@@ -12,5 +12,64 @@ function createDaysOfTheWeek() {
   };
   
   createDaysOfTheWeek();
-  
-  // Escreva seu código abaixo.
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  function renderDaysOfMonth(){
+    let getDayList=document.querySelector('#days');
+    for (let index=0;index<dezDaysList.length;index+=1){  //grantindo que todo o array de dias sera percorrido
+        let day =dezDaysList[index]; //pegando o dia da iteração atual
+        let dayItem=document.createElement('li'); // criando o elemento li correspondente.
+        if(day ===24|day === 31){ // tratando os dias que são feriados é renederizando
+            dayItem.className='day holiday'; //nomeando uma classe de acordo com o criteiro do exercício 
+            dayItem.innerHTML=day;
+            getDayList.appendChild(dayItem);
+        }else if(day === 4|day === 11| day === 18){ //tratando os dias que são sexta-feira é renderizando
+            dayItem.className='day  friday';
+            dayItem.innerHTML=day;
+            getDayList.appendChild(dayItem);
+        }else if(day === 25){ // tratando o dia que é feriado e sexta-feira é rederizando
+            dayItem.className='day holiday friday';
+            dayItem.innerHTML=day;
+            getDayList.appendChild(dayItem);
+        }else{ // rederizando todo o resto dos dias que são comuns 
+            dayItem.className='day';
+            dayItem.innerHTML=day;
+            getDayList.appendChild(dayItem);
+        }
+  }
+}
+  renderDaysOfMonth();
+/*mplemente uma função que receba como parâmetro a string "Feriados" e crie dinamicamente um botão com o nome "Feriados".
+Adicione a este botão a ID "btn-holiday" .
+Adicione este botão como filho/filha da tag <div> com classe "buttons-container" . */ 
+const buttonsContainer=document.querySelector('.buttons-container');
+let btnFeriado=document.createElement('button');
+btnFeriado.id='btn-holiday';
+btnFeriado.innerHTML='feriados';
+buttonsContainer.appendChild(btnFeriado);
+/* Implemente uma função que adicione ao botão "Feriados" um evento de "click" que muda a cor de fundo dos dias que possuem a classe "holiday" .
+É interessante que este botão possua também a lógica inversa. Ao ser clicado novamente ele retorna à configuração inicial com a cor "rgb(238,238,238)" .*/
+btnFeriado.addEventListener('click',function(){
+    const dayItemTarget=document.querySelectorAll('.day')
+    for(let index=0;index<dayItemTarget.length;index +=1){
+        let actualItem=dayItemTarget[index];
+    if(actualItem.className.includes('holiday')){
+        actualItem.style.color='red';
+    }
+}
+})
+/*Implemente uma função que receba como parâmetro a string "Sexta-feira" e crie dinamicamente um botão com o nome "Sexta-feira".
+Adicione a este botão o ID "btn-friday" .
+Adicione este botão como filho/filha da tag <div> com classe "buttons-container" . */
+let btnSexta = document.createElement('button');
+btnSexta.id='btn-friday';
+btnSexta.innerHTML="Sexta-Feira";
+buttonsContainer.appendChild(btnSexta)
+btnSexta.addEventListener('click',function(){
+    const target=document.querySelectorAll('.day');
+    for(let index=0;index<target.length;index+=1){
+        let actual=target[index];
+        if(actual.className.includes('friday')){
+            actual.innerHTML="Sexta-feira"
+        }
+    }
+})
--- Fundamentos/Bloco05:DOM,Eventos e Web Storage/5.3 JavaScript- Eventos/pratica/script.js
+++ Fundamentos/Bloco05:DOM,Eventos e Web Storage/5.3 JavaScript- Eventos/pratica/script.js
@@ -11,17 +11,40 @@ const myWebpage = document.getElementById('my-spotrybefy');
 
 // 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
+function handleChangeTech(event) {
+    const techElement = document.querySelector('.tech');
+    techElement.classList.remove('tech');
+    event.target.classList.add('tech');
+    input.value = '';
+  }
+  
+  firstLi.addEventListener('click', handleChangeTech);
+  secondLi.addEventListener('click', handleChangeTech);
+  thirdLi.addEventListener('click', handleChangeTech);
 
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
 
+  
+
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portifólio?
+myWebpage.addEventListener('dblclick', function() {
+    window.location.replace('https://raphaelmoraisdevmg.github.io/');
+  });
+
+
+
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
-
+myWebpage.addEventListener('mouseover',function(){
+    myWebpage.style.color="red";
+})
+myWebpage.addEventListener('mouseout',function(){
+    myWebpage.style.color="unset";
+})
 // Segue abaixo um exemplo do uso de event.target:
 
 
